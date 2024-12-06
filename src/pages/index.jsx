a4cbfad7aef7b3f13841287533c1f8705aafae0d
Suleyman <suley.kiani@gmail.com>
2024-12-05 21:05:50 -0500

    Updated resume

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -223,7 +223,7 @@ function Resume() {
           </li>
         ))}
       </ol>
-      <Button href="./Suleyman_Kiani_RESUME_2024.pdf" variant="secondary" className="group mt-6 w-full">
+      <Button href="./Suleyman_Kiani_RESUME_DEC_2024.pdf" variant="secondary" className="group mt-6 w-full">
         Download Resume
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
