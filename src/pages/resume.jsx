4671c7b0f4c3dfd580a722696fc4e1e665443b89
Suleyman <suley.kiani@gmail.com>
2025-02-11 17:05:34 -0500

    Resume page added

---
+++ src/pages/resume.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Document, Page } from 'react-pdf';
+import resumeFile from './Suleyman_Kiani_RESUME_2025.pdf';
+
+function Resume() {
+    return (
+        <div>
+            <h1>My Resume</h1>
+            <Document file={resumeFile}>
+                <Page pageNumber={1} />
+            </Document>
+        </div>
+    );
+}
+
+export default Resume;
